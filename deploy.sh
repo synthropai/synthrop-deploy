bac577f3dd4d8d2b8fc2f2c8a176d470256ddefd
DaviSM <davimesquita@gmail.com>
2025-07-14 19:15:51 -0300

    fix: ensure deploy script checks for root user and required /root directory

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 
 REPO=synthrop-cluster
+BRANCH=master
 
-#if [ "$(id -u)" -ne 0 ]; then
-#  echo "Error: This script must be run as root user"
-#  exit 1
-#fi FIXME check root user
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Error: This script must be run as root user"
+  exit 1
+fi
+
+if [ ! -d /root ]; then
+  echo "Error: /root directory does not exist"
+  exit 1
+fi
 
 if [ -z "${GITHUB_TOKEN}" ]; then
   echo "Error: GITHUB_TOKEN environment variable is not set"
@@ -15,6 +21,15 @@ fi
 if [ ! -e /root/synthrop.txt ]; then
   echo "Synthrop Initial..."
   echo "Cloning ${REPO} repository..."
+  cd /root
   git clone https://${GITHUB_TOKEN}@github.com/synthropai/${REPO}.git
+
+  echo "Running Deploy..."
+  cd ${REPO}
+  git reset --hard origin/$BRANCH
+
+  chmod +x deploy.sh
+  ./deploy.sh
 fi
 
+echo "Done."
