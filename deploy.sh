ca3c205d37e6acd2408f825c314fe678bf44d6aa
DaviSM <davimesquita@gmail.com>
2025-07-15 00:51:56 -0300

    fix: remove background execution operator to ensure proper script flow

--- deploy.sh
+++ deploy.sh
@@ -30,8 +30,8 @@ if [ ! -e /root/synthrop.txt ]; then
   git reset --hard origin/$BRANCH
 
   chmod +x deploy.sh
-  ./deploy.sh 2>&1 > ../deploy.log &
   tail -F ../deploy.log &
+  ./deploy.sh 2>&1 > ../deploy.log
 fi
 
 echo "Done."
